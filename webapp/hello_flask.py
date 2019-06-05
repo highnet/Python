02331fa0c405bd5f6fda37d3b076bd35f5f2640b
highnet <joaquintelleria@gmail.com>
2019-06-05 14:39:52 +0200

    Add files via upload

---
+++ webapp/hello_flask.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template
+from vsearch import search_for_letters
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello() -> str:
+    return 'Hello world from Flask!'
+
+@app.route('/search_for',methods=['POST'])
+def do_search() -> str:
+    return str(search_for_letters('life, the universe and everything', 'eiru.'))
+
+@app.route ('/entry')
+def entry_page() -> 'html':
+    return render_template('entry.html',
+                            the_title = "Welcome to the website for Idle Chess Story")
+
+app.run(debug = True)
