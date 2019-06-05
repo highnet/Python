0b549406150e369db2dcc59d1cdaff3cd01f6543
highnet <joaquintelleria@gmail.com>
2019-06-05 20:07:43 +0200

    Update classes.py

--- classes.py
+++ classes.py
@@ -1,11 +1,16 @@
 class CountFromBy:
-	def __init__(self, v: int, i:int) -> None:
-		self.val = v
-		self. incr = i
-		print('abc')
-	
-	def increase(self) -> None:
-		self.val += self.incr
-		
+    def __init__(self, v: int=0, i: int=1) -> None:
+        self.val = v
+        self.incr = i
+        self.increase()
 
-		
+    def __repr__(self) -> str:
+        return str(self.val)
+
+    def increase(self) -> None:
+        self.val += self.incr
+        print (self.val)
+
+a = CountFromBy(0,1)
+a.increase()
+print(a)
